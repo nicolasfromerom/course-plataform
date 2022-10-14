c9896200fd08c682f1190c8fe341f3d04a3f831e
Nicolas Fabricio Romero Morales <romero.nicolasone@gmail.com>
2022-10-14 08:59:28 -0500

    Migrations[B

---
+++ database/migrations/2022_10_14_131806_create_table_student_course.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_course', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_id')->constrained();
+            $table->foreignId('course_id')->constrained();
+            $table->integer('viewed_percentage');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_student_course');
+    }
+};
